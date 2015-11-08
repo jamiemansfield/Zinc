5cb0a2ec5da8009b5fbcc4c3801072a2c4b1a60a
Jamie Mansfield <malgm@lapis.blue>
2015-11-08 18:32:40 +0000

    Initial project structure

---
+++ src/main/java/uk/jamierocks/zinc/Command.java
@@ -0,0 +1,74 @@
+/*
+ * This file is part of Zinc, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2015, Jamie Mansfield <https://github.com/jamierocks>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package uk.jamierocks.zinc;
+
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.command.CommandSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This method should be applied to methods, defining a command.
+ *
+ * @author Jamie Mansfield
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Command {
+
+    /**
+     * Gets the name of the command.
+     *
+     * @return the command's name.
+     */
+    String name();
+
+    /**
+     * Gets all the aliases of this command.
+     *
+     * @return the command's aliases.
+     */
+    String[] aliases() default {};
+
+    /**
+     * Gets the description of this command.
+     * Converted to {@link Text}, using {@link Texts#of(String)}.
+     *
+     * @return the command's description.
+     */
+    String description() default "";
+
+    /**
+     * Gets all the permissions associated with this command.
+     * The {@link CommandSource} will have to have all permissions to be able to use the command.
+     * Not having any permission, means that all users can use the command, use with caution!
+     *
+     * @return the command's permissions.
+     */
+    String[] permissions() default {};
+}
